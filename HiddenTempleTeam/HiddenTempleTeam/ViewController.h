1561b7e9e703bb93a018d171c3f63917ebc16fd8
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-05-26 15:12:29 -0400

    XCode project created

---
+++ HiddenTempleTeam/HiddenTempleTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiddenTempleTeam
+//
+//  Created by Amy Joscelyn on 5/26/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
