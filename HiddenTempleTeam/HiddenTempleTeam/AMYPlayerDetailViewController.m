4db1882c4de476ab0b13083b571427d5cb470ded
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-06-03 17:04:05 -0400

    PlayerDetailVC: testing data with JSOG

--- HiddenTempleTeam/HiddenTempleTeam/AMYPlayerDetailViewController.m
+++ HiddenTempleTeam/HiddenTempleTeam/AMYPlayerDetailViewController.m
@@ -27,6 +27,8 @@
 {
     [super viewDidLoad];
     
+    NSLog(@"player: %@", self.player);
+    
     NSString *dateString = self.player[@"birthdate"];
     self.firstName = self.player[@"firstName"];
     self.lastName = self.player[@"lastName"];
@@ -41,7 +43,7 @@
     
     self.playerNameLabel.text = [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
     self.playerIDLabel.text = [NSString stringWithFormat:@"#%@", self.playerID];
-    self.birthdateLabel.text = [NSString stringWithFormat:@"birthdate: %@", self.birthdate];
+    self.birthdateLabel.text = [NSString stringWithFormat:@"born: %@", self.birthdate];
     
     //I want too to have a table within this view holding the teams (and dates hired) that player has been on, that when selected will segue to the appropriate team detail view.  I just need the team detail view first...
 }
